d6dbef89bf4bd443902fc4efa8a6d82348783d8c
Jose Fuentes <3307968951202@ingenieria.usac.edu.gt>
2023-03-28 21:19:20 -0600

    Actualizacion

---
+++ EDD_Proyecto1_Fase2/js/Nodo.js
@@ -0,0 +1,7 @@
+class Nodo_Usuarios{
+    constructor(dato, siguiente){
+        this.dato = dato;
+        this.siguiente = siguiente;
+    }
+}
+
---
+++ EDD_Proyecto1_Fase2/js/Usuarios.js
@@ -0,0 +1,8 @@
+class Usuario{
+    constructor(carne, nombre,password,carpetaRaiz){
+        this.carne = carne;
+        this.nombre = nombre;
+        this.password = password;
+        this.carpetaRaiz = carpetaRaiz;
+    }
+}
